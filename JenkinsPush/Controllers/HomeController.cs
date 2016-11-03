92de58ce5639128bf96c4963de8f098a71be3010
ViridianGroup <steven.graham@viridiangroup.co.uk>
2016-11-03 17:40:18 +0000

    FirstPush
    Make the first push

---
+++ JenkinsPush/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using JenkinsPush.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JenkinsPush.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            HomeViewModel model = new HomeViewModel("I am the header", "And i am the body");
+            return View(model);
+        }
+
+    }
+}
---
+++ JenkinsPush/Models/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JenkinsPush.Models
+{
+    public class HomeViewModel
+    {
+        public HomeViewModel(string header, string body)
+        {
+            this._header = header;
+            this._body = body;
+        }
+        public string _header { get; set; }
+        public string _body { get; set; }
+    }
+}
