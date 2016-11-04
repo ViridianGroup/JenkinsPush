c2157a1b14de66db28222e0d84677f3a8ccb97ce
ViridianGroup <steven.graham@viridiangroup.co.uk>
2016-11-04 10:44:18 +0000

    Tests
    Upload tests

---
+++ JenkinsPush.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JenkinsPush.Controllers;
+using System.Web.Mvc;
+
+namespace JenkinsPush.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FailingTestMethod() //
+        {
+            var controller = new HomeController();
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("Details", result.ViewName);
+        }
+
+        [TestMethod]
+        public void PassingTestMethod() //
+        {
+            var controller = new HomeController();
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+    }
+}
